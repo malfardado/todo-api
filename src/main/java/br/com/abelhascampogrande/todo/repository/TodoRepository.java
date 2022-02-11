8f8e35a7a7240ca98a75a50adfb0d1959afff8f7
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2022-02-10 21:02:35 -0300

    revert para projeto original

--- src/main/java/br/com/abelhascampogrande/todo/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.abelhascampogrande.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.abelhascampogrande.todo.model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-
-}
